100fd11e9d8ddc0a2d470e1b725dbc950c23bb22
getwaroo <gavishetwaroo@gmail.com>
2021-12-07 16:10:54 +0400

    main menu button

--- src/main/java/com/example/loginpage/CarpoolDashboardController.java
+++ src/main/java/com/example/loginpage/CarpoolDashboardController.java
@@ -55,6 +55,15 @@ public class CarpoolDashboardController {
         tableView_information.setItems(data);
     }
 
+    public void switchToMainMenu(MouseEvent e) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("loginPage.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public void switchToPoolingDashboard(MouseEvent e) throws IOException{
 
--- src/main/java/com/example/loginpage/CarpoolManagementController.java
+++ src/main/java/com/example/loginpage/CarpoolManagementController.java
@@ -137,6 +137,16 @@ public class CarpoolManagementController {
         stage.show();
     }
 
+    public void switchToMainMenu(MouseEvent e) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("loginPage.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public void getCellValue(MouseEvent e) throws IOException {
 
         CarpoolManagement carpoolManagement = tableView_management.getSelectionModel().getSelectedItem();
