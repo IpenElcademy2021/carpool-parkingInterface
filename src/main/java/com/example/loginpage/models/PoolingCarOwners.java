10aebdfd9950dd9e093bada997b3f7afa5c7fb27
gary <gary2203@live.com>
2021-12-07 10:16:27 +0400

    trying car  owners

---
+++ src/main/java/com/example/loginpage/models/PoolingCarOwners.java
@@ -0,0 +1,12 @@
+package com.example.loginpage.models;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PoolingCarOwners {
+
+    private @Getter @Setter String visa;
+    private @Getter @Setter String carPlate;
+}
